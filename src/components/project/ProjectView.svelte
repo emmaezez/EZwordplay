f70f81c7dbaf55e77a94baee8f34d1153536615f
Emma Zhang <zjy0124@uw.edu>
2026-03-11 18:39:05 -0700

    partial undo previous commit 75099dc

--- src/components/project/ProjectView.svelte
+++ src/components/project/ProjectView.svelte
@@ -1192,7 +1192,7 @@
             }
         }
         // Source? Cycle through source, expanding as necessary.
-        else if (currentTileIndex >= 0) {
+        else if (currentTileIndex) {
             const sources = layout.getSources();
             const index = sources.findIndex(
                 (source) => source.id === currentTile.id,
